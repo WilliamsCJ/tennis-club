55b040cd6c9acc4f9775835a1a11bfa9c3e4d8d2
CJ Williams <cj@cjwilliams.io>
2022-08-23 22:56:52 +0100

    Use favicon package

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" type="image/x-icon" href="avicon.ico" />
+        <link rel="icon" href="icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </Head>
       <body>
         <Main />
