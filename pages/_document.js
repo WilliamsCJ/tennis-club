dfed39eed31b8e03a31bad3909e28031dfc091cf
CJ Williams <cj@cjwilliams.io>
2022-08-23 23:01:07 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/x-icon" href="avicon.ico" />
-        <link rel="icon" href="icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
